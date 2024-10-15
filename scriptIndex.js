0aa9ffe41754b92bd7e5901a0036d62ed2ab1bea
AnaDuarteS <anacrisduartesantos20@gmail.com>
2024-10-15 09:36:23 -0300

    Inicio JS página Index

---
+++ scriptIndex.js
@@ -0,0 +1,8 @@
+let elementoNav = document.querySelector(".nav")
+console.log(elementoNav)
+
+let elementoMain = document.querySelector("main")
+console.log(elementoMain)
+
+let elementoFooter = document.querySelector("footer")
+console.log(elementoFooter)
